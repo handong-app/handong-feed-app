e12624e60bb75b7159be3e13010d1229a8006534
callendev20 <callendev20@gmail.com>
2025-03-29 01:49:42 +0900

    :sparkles: Implement CRUD of Tag entity - TagService, TagController

--- src/main/java/app/handong/feed/service/TagService.java
+++ src/main/java/app/handong/feed/service/TagService.java
@@ -3,8 +3,14 @@ package app.handong.feed.service;
 import app.handong.feed.dto.TagDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface TagService {
     TagDto.CreateResDto createTag(TagDto.CreateReqDto dto);
-    TagDto.DeleteResDto deleteTag(int id);
+    List<TagDto.CreateResDto> createTags(List<TagDto.CreateReqDto> requestList);
+    TagDto.ReadResDto readTag(String code);
+    List<TagDto.ReadResDto> readAllTags();
+    TagDto.UpdateResDto updateTag(String code, TagDto.UpdateReqDto dto);
+    TagDto.DeleteResDto deleteTag(String code);
 }
