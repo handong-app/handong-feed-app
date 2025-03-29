aea481bcf49f1caff5a421785ae7403ac11e6db1
callendev20 <callendev20@gmail.com>
2025-03-30 02:23:03 +0900

    :white_check_mark: Add /api-keys/{id}/toggle-status route - TbadminController

--- src/main/java/app/handong/feed/controller/TbadminController.java
+++ src/main/java/app/handong/feed/controller/TbadminController.java
@@ -36,4 +36,10 @@ public class TbadminController {
         String reqUserId = RequestUtils.getReqUserId(request);
         return ResponseEntity.ok(tbadminService.issueApiKey(reqUserId, dto));
     }
+
+    @PatchMapping("/api-keys/{id}/toggle-status")
+    public ResponseEntity<TbadminDto.ApiKeyDetail> toggleApiKeyStatus(@PathVariable Long id,HttpServletRequest request) {
+        String reqUserId = RequestUtils.getReqUserId(request);
+        return ResponseEntity.ok(tbadminService.toggleApiKeyStatus(reqUserId, id));
+    }
 }
