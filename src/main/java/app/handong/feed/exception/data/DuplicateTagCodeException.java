5e205216b0e2a4510ff38857289821caa7574456
callendev20 <callendev20@gmail.com>
2025-04-02 13:10:21 +0900

    📝 docs: add annotations and docs to DuplicateTagCodeException - Annotated DuplicateTagCodeException with @ResponseStatus(HttpStatus.CONFLICT) - Added @NoArgsConstructor for serialization and compatibility - Suppressed serialVersionUID warning using @SuppressWarnings(serial) - Documented the purpose of this exception via Javadoc

--- src/main/java/app/handong/feed/exception/data/DuplicateTagCodeException.java
+++ src/main/java/app/handong/feed/exception/data/DuplicateTagCodeException.java
@@ -1,7 +1,17 @@
 package app.handong.feed.exception.data;
 
-public class DuplicateTagCodeException extends RuntimeException {
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * 태그 생성시 중복 태그코드 (태그의 PK) 발생한 경우 사용되는 예외처리
+ * HttpStatus CONFLICT
+ */
+@ResponseStatus(value = HttpStatus.CONFLICT)
+@SuppressWarnings("serial")
+@NoArgsConstructor
+public class DuplicateTagCodeException extends RuntimeException {
     public DuplicateTagCodeException(String code) {
         super("이미 존재하는 태그 코드입니다: " + code);
     }
