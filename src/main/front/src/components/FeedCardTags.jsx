af70e71b2dccee643743ccf34304f828f4fba6c1
junglesub <junglesubmarine@gmail.com>
2025-04-16 00:05:25 +0900

    feat(client): space between tags

--- src/main/front/src/components/FeedCardTags.jsx
+++ src/main/front/src/components/FeedCardTags.jsx
@@ -4,29 +4,21 @@ import React from "react";
 function FeedCardTags({ tags }) {
   const theme = useTheme();
   return (
-    <Stack direction="row" spacing={1} sx={{ pt: 1 }}>
-      {tags && tags.length > 0 && (
-        <div
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            marginTop: "8px",
-          }}
-        >
-          {tags.map((tag) => (
-            <Chip
-              key={tag.code}
-              label={`#${tag.label}`}
-              sx={{
-                color: theme.palette.getContrastText(`#${tag.colorHex}`),
-                backgroundColor: `#${tag.colorHex}`,
-              }}
-              // color={tag.colorHex}
-              size="small"
-            />
-          ))}
-        </div>
-      )}
+    <Stack direction="row" spacing={1} sx={{ pt: 2 }}>
+      {tags &&
+        tags.length > 0 &&
+        tags.map((tag) => (
+          <Chip
+            key={tag.code}
+            label={`#${tag.label}`}
+            sx={{
+              color: theme.palette.getContrastText(`#${tag.colorHex}`),
+              backgroundColor: `#${tag.colorHex}`,
+            }}
+            // color={tag.colorHex}
+            size="small"
+          />
+        ))}
     </Stack>
   );
 }
