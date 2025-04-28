26b25d1808e5534a79884938a808c0c9bebec4c9
callendev20 <callendev20@gmail.com>
2025-04-28 23:45:12 +0900

    ♻️  refactor: apply @RequiredArgsConstructor to ExternalSubjectServiceImpl to reduce boilerplate

--- src/main/java/app/handong/feed/service/impl/ExternalSubjectServiceImpl.java
+++ src/main/java/app/handong/feed/service/impl/ExternalSubjectServiceImpl.java
@@ -4,15 +4,14 @@ import app.handong.feed.exception.data.NotFoundException;
 import app.handong.feed.mapper.TbsubjectMapper;
 import app.handong.feed.service.ExternalSubjectService;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ExternalSubjectServiceImpl implements ExternalSubjectService {
     private final TbsubjectMapper tbsubjectMapper;
 
-    public ExternalSubjectServiceImpl(TbsubjectMapper tbsubjectMapper) {
-        this.tbsubjectMapper = tbsubjectMapper;
-    }
 
     @Override
     @Transactional
