d929768a58fbacecb7e7cf860c30aee32574c931
Jungsub Ryoo <junglesubmarine@gmail.com>
2025-05-06 23:34:35 +0900

    feat: remove alerts (#159)

--- src/main/front/src/components/MainDisplay.jsx
+++ src/main/front/src/components/MainDisplay.jsx
@@ -17,39 +17,8 @@ function MainDisplay({ children, noCount = false }) {
       <MainDrawer noCount={noCount} />
       <Box
         component="main"
-        sx={{ flexGrow: 1, px: 1.5, pb: 6, maxWidth: 700, mx: "auto" }}
+        sx={{ flexGrow: 1, px: 1.5, pt: 3, pb: 6, maxWidth: 700, mx: "auto" }}
       >
-        <Alert severity="info" sx={{ borderRadius: 0, my: 1 }}>
-          <AlertTitle>
-            우측 하단 '모두 읽음 처리' 기능 추가되었습니다{" "}
-            <a
-              style={{ color: "black" }}
-              href="https://board.handong.app/feed/num/33"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              (#33)
-            </a>
-          </AlertTitle>
-          우측 하단에 알림을 한 번에 모두 읽음으로 변경할 수 있는 기능이
-          추가되었습니다.
-        </Alert>
-        <Alert severity="warning" sx={{ borderRadius: 0, my: 1 }}>
-          <AlertTitle>
-            현재 새로운 피드가 <b>늦게</b> 올라가는 오류가 있습니다.
-            <a
-              style={{ color: "black" }}
-              href="https://board.handong.app/feed/num/31"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              (#31)
-            </a>
-          </AlertTitle>
-          개발팀에서 하루에 한 번 수동으로 추가하고 있으니 조금만 기다려 주세요.
-          이용에 불편을 드려 죄송합니다.
-        </Alert>
-
         {children}
       </Box>
       {/* <Button variant="contained">Hello world</Button> */}
