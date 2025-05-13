809e7ea03768f85c1806c823227d67169539987e
callendev20 <callendev20@gmail.com>
2025-05-13 16:38:59 +0900

    ✨ feat: add custom setter and Swagger docs for onlyUnassignedFeeds filter - ExternalDto.FeedReqDto

--- src/main/java/app/handong/feed/dto/ExternalDto.java
+++ src/main/java/app/handong/feed/dto/ExternalDto.java
@@ -14,13 +14,20 @@ public class ExternalDto {
         Long start, end;
         Integer limit;
 
+        @Schema(description = "신규 피드만 필터링할지 여부")
         boolean isFilterNew;
+        @Schema(description = "할당되지 않은 피드만 필터링할지 여부")
         boolean onlyUnassignedFeeds;
 
 
         // 커스텀 setter
         public void setIsFilterNew(String value) {
             this.isFilterNew = "1".equals(value);
+        }
+
+        // onlyUnassignedFeeds 커스텀 setter
+        public void setOnlyUnassignedFeeds(String value) {
+            this.onlyUnassignedFeeds = "1".equals(value);
         }
     }
 }
