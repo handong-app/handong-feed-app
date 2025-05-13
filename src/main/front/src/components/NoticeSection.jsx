073fceea10801f2dfd10cb938e78c37a6a9b3a53
junglesub <junglesubmarine@gmail.com>
2025-05-13 14:48:28 +0900

    :sparkles: feat: add hisnet notice

--- src/main/front/src/components/NoticeSection.jsx
+++ src/main/front/src/components/NoticeSection.jsx
@@ -7,17 +7,17 @@ function NoticeSection({ notices }) {
       <Typography variant="h5" sx={{ mb: 2, fontWeight: "bold" }}>
         📚 히즈넷 공지
       </Typography>
-      <Box sx={{ backgroundColor: "#f5f5f5", p: 1, mb: 2 }}>
+      <Box sx={{ backgroundColor: "#f5f5f5", p: 1, mb: 1 }}>
         <Typography variant="subtitle1">전체 히츠넷 공지사항</Typography>
       </Box>
-      <Typography variant="caption" gutterBottom display="block">
-        * 글배수가 기준 글은 서지를 들어갑니다.
+      <Typography variant="caption" gutterBottom display="block" mb={2}>
+        * 조회수 기준으로 정렬되어 있습니다.
       </Typography>
-      <Typography variant="subtitle2" color="primary" gutterBottom>
+      <Typography variant="subtitle2" color="primary">
         드롭합늘관리부 교심전문
       </Typography>
       {notices.map((notice) => (
-        <Box key={notice.id} sx={{ my: 2 }}>
+        <Box key={notice.id} sx={{ mb: 2 }}>
           <Typography variant="body2" component="div">
             {notice.title}
           </Typography>
